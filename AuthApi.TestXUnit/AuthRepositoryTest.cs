1ed9ddca02213677bbe5a1c3b075ab25aff39139
Henry <jm25002@esfe.agape.edu.sv>
2025-10-15 21:21:27 -0600

    Se ha creado un nuevo proyecto de pruebas "AuthApi.TestXunit" en la solución, incluyendo su configuración de compilación. Se ha implementado la clase `AuthRepositoryTest` con pruebas para el registro y el inicio de sesión de usuarios, y la clase `UsuarioRepositoryTest` para pruebas de operaciones de usuario, utilizando un repositorio simulado y una base de datos en memoria.

---
+++ AuthApi.TestXUnit/AuthRepositoryTest.cs
@@ -0,0 +1,94 @@
+﻿using AuthApi.DTOs.UsuarioDTOs;
+using AuthApi.Entidades;
+using AuthApi.Interfaces;
+using AuthApi.Repositories;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthApi.TestXunit
+{
+    public class AuthRepositoryTest
+    {
+   
+        private IConfiguration GetTestConfiguration()
+        {
+            var inMemoryConfig = new Dictionary<string, string>
+        {
+            { "Jwt:Key", "ClaveSuperSecretaMuyLargaTest5674321" },
+            { "Jwt:Issuer", "AuthApiTest" },
+            { "Jwt:Audience", "AuthApiClientes" },
+        };
+
+            return new ConfigurationBuilder().AddInMemoryCollection(inMemoryConfig).Build();
+        }
+        [Fact]
+        public async Task RegistrarAsync_RetorneUsuarioConToken()
+        {
+            //Arrange
+            var mockRepo = new Mock<IUsuarioRepository>();
+            var config = GetTestConfiguration();
+            var usuario = new Usuario
+            {
+                Id = 1,
+                Nombre = "henry",
+                Email = "henry@test.com",
+                PasswordHash = "hash",
+                Rol = new Rol { Id = 2, Nombre = "Usuario" }
+            };
+
+            mockRepo.Setup(r => r.AddAsync(It.IsAny<Usuario>())).ReturnsAsync(usuario);
+
+            mockRepo.Setup(r => r.GetByEmailAsync(It.IsAny<string>())).ReturnsAsync(usuario);
+
+            var service = new AuthRepository(mockRepo.Object, config);
+
+            var registroDTO = new UsuarioRegistroDTO
+            {
+                Nombre = "henry",
+                Email = "henry@test.com",
+                Password = "123"
+            };
+
+            //Act
+            var result = await service.RegistrarAsync(registroDTO);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal("henry", result.Nombre);
+            Assert.Equal("henry@test.com", result.Email);
+            Assert.False(string.IsNullOrEmpty(result.Token));
+
+
+        }
+
+        [Fact]
+        public async Task LoginAsync_RetornaNullSiUsuarioNoExiste()
+        {
+            // Arrange
+            var mockRepo = new Mock<IUsuarioRepository>();
+            var config = GetTestConfiguration();
+
+            mockRepo.Setup(r => r.GetByEmailAsync(It.IsAny<string>()))
+                    .ReturnsAsync((Usuario?)null);
+
+            var service = new AuthRepository(mockRepo.Object, config);
+
+            var loginDTO = new UsuarioLoginDto
+            {
+                Email = "walternoexiste@test.com",
+                Password = "123",
+            };
+
+            // Act
+            var result = service.LoginAsync(loginDTO);
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ AuthApi.TestXUnit/UsuarioRepositoryTest.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuthApi.Entidades;
+using AuthApi.Repositorios;
+
+
+namespace AuthApi.TestXunit
+{
+    public class UsuarioRepositoryTest
+    {
+
+        private AppDbContext GetInMemoryDbContext()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: $"TestDB_{System.Guid.NewGuid()}").Options;
+
+            var context = new AppDbContext(options);
+
+            context.Roles.Add(
+                new Rol { Id = 1, Nombre = "Admin" }
+            );
+
+            context.Usuarios.Add(
+                new Usuario
+                {
+                    Id = 2,
+                    Nombre = "henry",
+                    Email = "henry@test.com",
+                    PasswordHash = "123",
+                    RolId = 1
+                }
+            );
+
+            context.SaveChanges();
+            return context;
+        }
+
+        [Fact]
+        public async Task GetByEmailAsync_RetornarUsuarioExistente()
+        {
+            //Arrange
+            var context = GetInMemoryDbContext();
+            var repo = new UsuarioRepository(context);
+
+            //Act
+            var usuario = await repo.GetByEmailAsync("henry@test.com");
+
+            //Assert
+            Assert.NotNull(usuario);
+            Assert.Equal("henry", usuario.Nombre);
+            Assert.Equal("Admin", usuario.Rol.Nombre);
+        }
+        [Fact]
+        public async Task AddAsync_AgregarUsuario()
+        {
+            //Arrange
+            var context = GetInMemoryDbContext();
+            var repo = new UsuarioRepository(context);
+
+            var nuevoUsuario = new Usuario()
+            {
+                Nombre = "eduardo",
+                Email = "eduardo@test.com",
+                PasswordHash = "123",
+                RolId = 1
+            };
+
+            //Act
+            await repo.AddAsync(nuevoUsuario);
+
+            //Assert
+            var usuarioGuardado = await context.Usuarios.FirstOrDefaultAsync(u => u.Email == "eduardo@test.com");
+
+            Assert.NotNull(usuarioGuardado);
+            Assert.Equal("eduardo", usuarioGuardado.Nombre);
+        }
+        [Fact]
+
+        public async Task GetAllUsuariosAsync_RetornaListaUsuarios()
+        {
+            //Arrange
+            var context = GetInMemoryDbContext();
+            var repo = new UsuarioRepository(context);
+
+            //Act
+            var lista = await repo.GetAllUsuariosAsync();
+
+            //Assert
+            Assert.NotEmpty(lista);
+            Assert.Contains(lista, u => u.Email == "henry@test.com");
+        }
+    }
+}
